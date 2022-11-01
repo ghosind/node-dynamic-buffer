f001b454cbee504127d7fef61ddf8176c3cf5bf1
ghosind <ghosind@gmail.com>
2022-11-01 20:01:39 +0800

    feat: add toString method.

--- src/dynamicBuffer.ts
+++ src/dynamicBuffer.ts
@@ -47,6 +47,29 @@ export class DynamicBuffer {
     return Buffer.from(this.buffer, 0, this.used);
   }
 
+  /**
+   * Decodes buffer to a string with the specified character encoding and range.
+   * @param encoding The character encoding to use, default from buffer encoding.
+   * @param start The byte offset to start decoding at, default 0.
+   * @param end The byte offset to stop decoding at (not inclusive), default used bytes offset.
+   * @returns The string decodes from buffer with the specified range.
+   */
+  toString(encoding?: BufferEncoding, start?: number, end?: number) {
+    let startOffset = start;
+    let endOffset = end;
+
+    if (!startOffset || startOffset < 0) {
+      startOffset = 0;
+    } else if (startOffset > this.used) {
+      startOffset = this.used;
+    }
+    if (endOffset === undefined || endOffset > this.used) {
+      endOffset = this.used;
+    }
+
+    return this.buffer.toString(encoding || this.encoding, startOffset, endOffset);
+  }
+
   private resize() {
     const newBuffer = Buffer.alloc(this.size * 2, this.fill, this.encoding);
     this.buffer.copy(newBuffer, 0, 0, this.used);
